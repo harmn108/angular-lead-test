bca55c64fa7389916d2664901dfe3ef0afe34655
Harutyun Mnatsakanyan <harmn90@gmail.com>
2020-07-28 18:25:44 +0400

    bug fixing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,33 +18,26 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.initDefaultData();
+  }
+
+  initDefaultData() {
     this.groupsList.push(
       new Group({name: 'Group 1', groupItems: [
-          {default: true, image: '', name: 'aaa', price: '5$', inStock: false, subCategory: true},
-          {default: false, image: '', name: 'bbb', price: '6$', inStock: true, subCategory: false},
-          {default: true, image: '', name: 'ccc', price: '7$', inStock: false, subCategory: true},
-        ], showImages: true}),
+        {default: true, image: '', name: 'aaa', price: '5$', inStock: false, subCategory: true},
+        {default: false, image: '', name: 'bbb', price: '6$', inStock: true, subCategory: false},
+        {default: true, image: '', name: 'ccc', price: '7$', inStock: false, subCategory: true},
+      ], showImages: true}),
       new Group({name: 'Group 2', groupItems: [
-          {default: false, image: '', name: 'Helium', price: '4.0026$', inStock: true, subCategory: false},
-        ]}),
+        {default: false, image: '', name: 'Helium', price: '4.0026$', inStock: true, subCategory: false},
+      ]}),
       new Group({name: 'Group 3', groupItems: [
-          {default: false, image: '', name: 'Lithium', price: '6.941$', inStock: false, subCategory: true}
-        ]})
+        {default: false, image: '', name: 'Lithium', price: '6.941$', inStock: false, subCategory: true}
+      ]})
     );
   }
 
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.groupsList, event.previousIndex, event.currentIndex);
-  }
-
-  dropTable(event: CdkDragDrop<any[]>, groupItems) {
-    moveItemInArray(groupItems, event.previousIndex, event.currentIndex);
-  }
-
-  getTableData(data) {
-    return new MatTableDataSource(data);
-  }
-
+  // Group Block Actions
   addNewGroup() {
     this.groupsList.push(new Group({name: `Group ${this.groupsList.length + 1}`}));
   }
@@ -55,8 +48,21 @@ export class AppComponent implements OnInit {
     }
   }
 
+  dropGroup(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.groupsList, event.previousIndex, event.currentIndex);
+  }
+
+  // Group Table Actions
+  getTableData(data) {
+    return new MatTableDataSource(data);
+  }
+
   addNewTableItem(groupItems) {
     groupItems.push({default: true, image: '', name: 'nextImage', price: '1.0079$', inStock: true, subCategory: true});
     this.ref.detectChanges();
+  }
+
+  dropTable(event: CdkDragDrop<any[]>, groupItems) {
+    moveItemInArray(groupItems, event.previousIndex, event.currentIndex);
   }
 }
